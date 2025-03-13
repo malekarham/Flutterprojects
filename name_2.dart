23af8c3a4126bbd5d97839ea4cf063b3deaaecb7
Muhammad Arham Maqsood <malekmuhammadarhum@gmail.com>
2025-03-13 09:36:50 -0700

    Delete name_2.dart

--- name_2.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    const Center(
-      child: Text('Muhammad Arham Maqsood', textDirection: TextDirection.ltr),
-    ),
-  );
-}
